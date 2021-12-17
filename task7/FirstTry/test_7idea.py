f2af658c068af44dede09a42afa2d26c50a05994
Oleksandra Bereza <oleksandra_bereza@epam.com>
2021-12-17 12:54:29 +0200

    hint for generating html report

--- task7/FirstTry/test_7idea.py
+++ task7/FirstTry/test_7idea.py
@@ -2,6 +2,12 @@ from task7idea import division
 import pytest
 
 
+# $py -m pytest --html=report.html test_7idea.py
+# Run from terminal for generating html report
+# report.html - where 'report' is name of file
+# more info see on https://pytest-html.readthedocs.io/en/latest/user_guide.html
+
+
 @pytest.mark.parametrize("a, b, expected_result", [(10, 2, 5),
                                                    (20, 2, 0),
                                                    (30, -3, -10),
